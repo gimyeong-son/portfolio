f1aba713b347b26aeae503ff9d4377aae9459358
Gimyeong <dev.gmson@gmail.com>
2025-04-02 15:26:46 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -16,3 +16,25 @@ const homeHeight = home.offsetHeight;
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// Arrow up 버튼을 아래로 스크롤시 투명하게 처리한다.
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
+
+// Navbar 토글버튼 클릭 처리
+const navbarMenu = document.querySelector(".header__menu");
+const navbarToggle = document.querySelector(".header__toggle");
+navbarToggle.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
+
+// Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener("click", () => {
+  navbarMenu.classList.remove("open");
+});
