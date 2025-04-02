3bef57aecd86b090a0470dab0e4d098f7edb3fea
Gimyeong <dev.gmson@gmail.com>
2025-04-02 15:31:04 +0900

    JS-Files

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+"use strict";
 // Header에 페이지 아래로 스크롤시 다크 스타일링을 적용한다.
 const header = document.querySelector(".header");
 // const headerHeight = header.getBoundingClientRect().height; // 소수점까지 정확하게 알려준다.
